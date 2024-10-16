aac9521e91e19cd5e23c005c44d2ee9ba5631170
gabok2 <gabrielbld9@gmail.com>
2024-10-16 12:14:42 -0300

    feat: adicionado component para input select

--- src/app/components/ui/InputSelect.tsx
+++ src/app/components/ui/InputSelect.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import {
   Label,
   Listbox,
@@ -8,59 +7,76 @@ import {
   ListboxOption,
   ListboxOptions,
 } from "@headlessui/react";
+import { Controller, FieldValues, Path, Control } from "react-hook-form";
+import { Icons } from "./icons";
 
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-  { id: 7, name: "Caroline Schultz" },
-  { id: 8, name: "Mason Heaney" },
-  { id: 9, name: "Claudie Smitham" },
-  { id: 10, name: "Emil Schaefer" },
-];
+interface InputSelectProps<TFieldValues extends FieldValues> {
+  name: Path<TFieldValues>;
+  arrayItens: { id: number; name: string }[];
+  control: Control<TFieldValues>;
+  label: string;
+}
 
-export function InputSelect() {
-  const [selected, setSelected] = useState(people[3]);
-
+export function InputSelect<TFieldValues extends FieldValues>({
+  name,
+  arrayItens,
+  control,
+  label,
+}: Readonly<InputSelectProps<TFieldValues>>) {
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      <Label className="block text-sm font-medium text-gray-700 mb-1">
-        Assigned to
-      </Label>
-      <div className="relative ">
-        <ListboxButton className="flex items-center border border-grayPrimary rounded-lg p-4 w-full">
-          <span className=" text-xs text-gray-400 bg-inherit focus:outline-none">
-            {selected.name}
-          </span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-            {/* <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" /> */}
-          </span>
-        </ListboxButton>
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => {
+        const selectedPerson = arrayItens.find(
+          (item) => item.id === field.value
+        );
 
-        <ListboxOptions
-          transition
-          className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
-        >
-          {people.map((person) => (
-            <ListboxOption
-              key={person.id}
-              value={person}
-              className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-            >
-              <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                {person.name}
-              </span>
+        return (
+          <Listbox
+            value={selectedPerson}
+            onChange={(item) => field.onChange(item.id)}
+          >
+            <Label className="block text-sm font-medium text-gray-700 mb-1">
+              {label}
+            </Label>
+            <div className="relative">
+              <ListboxButton className="flex items-center border border-grayPrimary rounded-lg p-4 w-full">
+                <span className="text-xs text-gray-400 bg-inherit focus:outline-none">
+                  {selectedPerson?.name}
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <Icons
+                    type="CaretDown"
+                    size={20}
+                    aria-hidden="true"
+                    className="text-gray-400"
+                  />
+                </span>
+              </ListboxButton>
 
-              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                {/* <CheckIcon aria-hidden="true" className="h-5 w-5" /> */}
-              </span>
-            </ListboxOption>
-          ))}
-        </ListboxOptions>
-      </div>
-    </Listbox>
+              <ListboxOptions
+                transition
+                className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+              >
+                {arrayItens.map((item) => (
+                  <ListboxOption
+                    key={item.id}
+                    value={item}
+                    className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-primary data-[focus]:text-white"
+                  >
+                    <span className="block truncate font-normal group-data-[selected]:font-semibold">
+                      {item.name}
+                    </span>
+
+                    <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-primary group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden"></span>
+                  </ListboxOption>
+                ))}
+              </ListboxOptions>
+            </div>
+          </Listbox>
+        );
+      }}
+    />
   );
 }
